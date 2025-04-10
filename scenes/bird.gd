3bab72f8d80d68b327374b67ae70af71fde7a28e
codingdemon9 <Michaeldunnemail@gmail.com>
2025-04-10 11:58:17 -0700

    fixed jumping

--- scenes/bird.gd
+++ scenes/bird.gd
@@ -8,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x -= get_parent().speed / 1.8
+	position.x -= get_parent().speed / 2.4
